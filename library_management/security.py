fa2905f9d5c26064c2909b5a677fe591c340eebc
Miguel-Marquess <mluiz4784@gmail.com>
2026-06-28 16:44:10 -0300

    feat: criada as funcooes que criam e validam o jwt token, alem da funcao get_current_uset, que bloqueia as rotas e exige autenticacao para utiliza-las.

--- library_management/security.py
+++ library_management/security.py
@@ -1,10 +1,72 @@
-from pwdlib import PasswordHash
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
+from fastapi import Depends
+from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jwt import decode, encode
+from jwt.exceptions import DecodeError
+from pwdlib import PasswordHash
+from sqlalchemy import select
+
+from library_management.depends.database_dependencies import Session
+from library_management.models.db_models import UserDatabase
+from library_management.settings import Settings
+
+settings = Settings()
 pwd_context = PasswordHash.recommended()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/login')
 
 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
 
+
 def verify_password(pure_password: str, hashed_password: str):
     return pwd_context.verify(pure_password, hashed_password)
+
+
+def create_access_token(claims: dict):
+    to_encode = claims.copy()
+
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES_TIME
+    )
+
+    to_encode.update({'exp': expire})
+
+    encoded_token = encode(to_encode, settings.TOKEN_SECRET_KEY, settings.ALGORITHM)
+
+    return encoded_token
+
+
+async def get_current_user(
+    session: Session, access_token: str = Depends(oauth2_scheme)
+):
+
+    def decode_bearer_token(jwt: dict):
+        credentials = decode(jwt, settings.TOKEN_SECRET_KEY, settings.ALGORITHM)
+        if not credentials.get('sub') or not credentials.get('exp'):
+            raise invalid_credentials
+        return credentials
+
+    invalid_credentials = HTTPException(
+        status_code=401,
+        detail='Credentials cannot be validateds.',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+
+    try:
+        payload = decode_bearer_token(access_token)
+    except DecodeError:
+        raise invalid_credentials
+
+    user = await session.scalar(
+        select(UserDatabase).where(UserDatabase.email == payload.get('sub'))
+    )
+
+    if not user:
+        raise invalid_credentials
+
+    return user
